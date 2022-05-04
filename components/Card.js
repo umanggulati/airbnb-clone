79ab1937dd3c4294e28a36e4d345facff2dd08dc
umanggulati <41832270+umanggulati@users.noreply.github.com>
2022-05-05 04:58:19 +0530

    Add files via upload

---
+++ components/Card.js
@@ -0,0 +1,27 @@
+
+import React from "react"
+import d from "../images/d.png"
+
+
+export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    return (
+        <div className="card">
+             {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={props.coverImg} className="card--image" />
+            <div className="card--stats">
+                <img src={d} className="card--star" />
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
+        </div>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+import a from "../images/a.png"
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={a} className="nav--logo" />
+        </nav>
+    )
+}
